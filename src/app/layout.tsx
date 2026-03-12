374250fdf2b2d92c8172b9b0d217f016a6e581d2
Manuel-Montaruli <manuelmontaruli@gmail.com>
2026-03-12 01:09:03 +0100

    Cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
-        {children}
+        {/* Navbar */}
+        <div className="w-full grow">
+            {children}
+        </div>
+        {/* Footer */}
       </body>
     </html>
   );
